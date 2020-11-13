142f55fe125a2984fd6fc2d0da72681b3f58d485
gradowli <gradowli@iu.edu>
2020-11-13 09:03:32 -0500

    small changes

--- tests2/r4_02.rkt
+++
@@ -1,3 +0,0 @@
-(define (add8  [a : Integer] [b : Integer] [c : Integer] [d : Integer] [e : Integer] [f : Integer] [g : Integer] [h : Integer]) : Integer
-   (+ a (+ b (+ c (+ d (+ e (+ f (+ g h))))))))
-(add8 1 2 3 4 5 6 7 8)
